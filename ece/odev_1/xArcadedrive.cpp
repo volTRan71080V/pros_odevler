4e9ae62ae1e70749e9805c150639fb9363eaba6e
Hacke333 <yagizegeurtac@gmail.com>
2023-08-09 17:17:13 +0300

    ece ve serhat hamza odev1

---
+++ ece/odev_1/xArcadedrive.cpp
@@ -0,0 +1,16 @@
+#include "okapi/api.hpp"
+using namespace okapi;
+
+std::shared_ptr<ChassisController> drive =
+    ChassisControllerBuilder()
+        .withMotors(1, -2, -3, 4)
+        .withDimensions(AbstractMotor::gearset::green,
+                        {{4_in, 13.5_in}, imev5GreenTPR})
+        .withOdometry()
+        .buildOdometry();
+
+void OpControl() {
+  drive->getModel()->xArcade(controller.getAnalog(ControllerAnalog::rightX),
+                             controller.getAnalog(ControllerAnalog::rightY),
+                             controller.getAnalog(ControllerAnalog::leftX));
+}
---
+++ serhat/odev_1/tankdrive.cpp
@@ -0,0 +1,15 @@
+#include "okapi/api.hpp"
+using namespace okapi;
+
+std::shared_ptr<ChassisController> drive =
+    ChassisControllerBuilder()
+        .withMotors({1, 2, 3, 4}, {-5, -6, -7, -8})
+        .withDimensions(AbstractMotor::gearset::green,
+                        {{4_in, 13.5_in}, imev5GreenTPR})
+        .withOdometry()
+        .buildOdometry();
+
+void OpControl() {
+  drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
+                          controller.getAnalog(ControllerAnalog::rightX));
+}
